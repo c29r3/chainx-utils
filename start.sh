404d67ee0f08642579615284a2131bdbde69dde7
Sergey <52304443+c29r3@users.noreply.github.com>
2020-11-09 19:00:39 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ cd /root/chainx; \
 wget -O /root/go/bin/chainx https://github.com/chainx-org/ChainX/releases/download/v2.0.0-beta.2/chainx-v2.0.0-beta.2-ubuntu-18.04-x86_64-unknown-linux-gnu; \
 chmod u+x /root/go/bin/chainx; \
 wget https://raw.githubusercontent.com/c29r3/chainx-utils/main/chainx.service; \
-echo "/root/go/bin/chainx --chain=testnet --validator --name $(cat /root/chainx/acc.txt)" > /root/chainx/start.sh; \
+echo -e "#!/bin/bash\n/root/go/bin/chainx --chain=testnet --validator --name $(cat /root/chainx/acc.txt)" > /root/chainx/start.sh; \
 chmod u+x /root/chainx/start.sh; \
 ln -sf /root/chainx/chainx.service /etc/systemd/system/; \
 systemctl daemon-reload; \
