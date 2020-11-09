d74cd98c8fee7539188140a71d06d1cd3aa84214
Sergey <52304443+c29r3@users.noreply.github.com>
2020-11-09 21:48:21 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,6 @@
 
 mkdir /root/chainx; \
 wget -O /root/go/bin/chainx https://github.com/chainx-org/ChainX/releases/download/v2.0.0-beta.2/chainx-v2.0.0-beta.2-ubuntu-18.04-x86_64-unknown-linux-gnu; \
-mv /root/go/bin/chainx-v2.0.0-beta.2-ubuntu-18.04-x86_64-unknown-linux-gnu /root/go/bin/chainx; \
 chmod u+x /root/go/bin/chainx; \
 curl -s https://raw.githubusercontent.com/c29r3/chainx-utils/main/chainx.service > /root/chainx/chainx.service; \
 echo -e "#!/bin/bash\n/root/go/bin/chainx --chain=testnet --validator --name $(cat /root/chainx/acc.txt)" > /root/chainx/start.sh; \
