22d426bec8b46e562cc3bff27d98a32fb5fbb913
Sergey <52304443+c29r3@users.noreply.github.com>
2020-11-26 03:57:19 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,11 +3,11 @@
 mkdir /root/chainx; \
 wget -O /root/go/bin/chainx https://github.com/chainx-org/ChainX/releases/download/v2.0.1/chainx-v2.0.1-ubuntu-18.04-x86_64-unknown-linux-gnu; \
 chmod u+x /root/go/bin/chainx; \
-curl -s https://raw.githubusercontent.com/c29r3/chainx-utils/main/chainx.service > /root/chainx/chainx.service; \
+# curl -s https://raw.githubusercontent.com/c29r3/chainx-utils/main/chainx.service > /root/chainx/chainx.service; \
 systemctl stop chainx; \
 echo -e "#!/bin/bash\n/root/go/bin/chainx --chain=mainnet --validator --name $(cat /root/chainx/acc.txt)" > /root/chainx/start.sh; \
 chmod u+x /root/chainx/start.sh; \
-ln -sf /root/chainx/chainx.service /etc/systemd/system/; \
+# ln -sf /root/chainx/chainx.service /etc/systemd/system/; \
 rm -rf /root/.local/share/chainx; \
 systemctl daemon-reload; \
 systemctl enable chainx; \
